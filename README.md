3b2eedbc3c5265b59db57a7dcccf34473b12f27c
Luke Horvat <lukehorvat@gmail.com>
2015-06-04 00:20:56 +1000

    Added 'recursive' option

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # computed-style-to-inline-style [![NPM version](http://img.shields.io/npm/v/computed-style-to-inline-style.svg?style=flat-square)](https://www.npmjs.org/package/computed-style-to-inline-style)
 
-Convert a HTML element's computed CSS to inline CSS. Uses [Window.getComputedStyle](https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle) internally.
+Convert a HTML element's computed CSS to inline CSS.
+
+Uses [Window.getComputedStyle](https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle) internally.
 
 ## Installation
 
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function computedToInline(element) {
-  [].forEach.call(element.children, function(child) {
-    computedToInline(child);
-  });
+function computedToInline(element, recursive) {
+  if (recursive) {
+    [].forEach.call(element.children, function(child) {
+      computedToInline(child, recursive);
+    });
+  }
 
   var computedStyle = getComputedStyle(element, null);
   for (var i = 0; i < computedStyle.length; i++) {
@@ -9,6 +11,6 @@ function computedToInline(element) {
     var value = computedStyle.getPropertyValue(property);
     element.style[property] = value;
   }
-};
+}
 
 module.exports = computedToInline;
