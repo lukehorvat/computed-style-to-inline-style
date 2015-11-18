6081b8a8c977c46628fbda75279c3f16c80257dd
Luke Horvat <lukehorvat@gmail.com>
2015-11-19 02:39:59 +1100

    Implement rudimentary unit test

---
+++ test.js
@@ -0,0 +1,19 @@
+var page = require("webpage").create();
+page.content = "<html><body><h1 id='test' style='color: red'></h1></body></html>";
+page.injectJs("index.js");
+
+page.evaluate(function() {
+  var element = document.getElementById("test");
+
+  if (element.style.length !== 1 || element.style[0] !== "color" || element.style["color"] !== "red") {
+    throw new Error("Invalid inline style before.");
+  }
+
+  computedStyleToInlineStyle(element);
+
+  if (element.style.length <= 1) {
+    throw new Error("Invalid inline style after.");
+  }
+});
+
+phantom.exit();
