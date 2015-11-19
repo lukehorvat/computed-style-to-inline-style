e1e861cf95ece18a6f619fd13f55f5c5a5307317
Luke Horvat <lukehorvat@gmail.com>
2015-11-19 21:55:41 +1100

    Use Husky

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var page = require("webpage").create();
 page.content = "<html><body><h1 id='test' style='color: red'></h1></body></html>";
 page.injectJs("index.js");
 
-page.evaluate(function() {
+var success = page.evaluate(function() {
   var element = document.getElementById("test");
 
   if (element.style.length !== 1 || element.style[0] !== "color" || element.style["color"] !== "red") {
@@ -14,7 +14,8 @@ page.evaluate(function() {
   if (element.style.length <= 1) {
     throw new Error("Invalid inline style after.");
   }
+
+  return true;
 });
 
-console.log("Done!");
-phantom.exit();
+phantom.exit(success ? 0 : 1);
